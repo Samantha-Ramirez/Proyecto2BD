4e1b91c1bc000495f13aada6df3b5c13b288c24f
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2025-02-19 01:39:41 -0400

    Restricciones para tablas 3

--- tablas.sql
+++ tablas.sql
@@ -1,38 +1,38 @@
 -- Paises 
 CREATE TABLE Pais(
     id INT,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
     PRIMARY KEY(id)
 );
 
 -- Estados 
 CREATE TABLE Estado(
     id INT,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
     paisId INT,
-    FOREIGN KEY(paisId) REFERENCES Pais(id)
+    FOREIGN KEY(paisId) REFERENCES Pais(id),
     PRIMARY KEY(id)
 );
 
 -- Ciudades 
 CREATE TABLE Ciudad(
     id INT,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
     estadoId INT,
-    FOREIGN KEY(estadoId) REFERENCES Estado(id)
+    FOREIGN KEY(estadoId) REFERENCES Estado(id),
     PRIMARY KEY(id)
 );
 
 -- Sucursales 
 CREATE TABLE Sucursal(
     id INT,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
     direccion VARCHAR(100),
     telefono VARCHAR(20),
-    horaAbrir TIME,
-    horaCerrar TIME,
+    horaAbrir INT CHECK (horaAbrir >= 0 AND horaAbrir <= 23),
+    horaCerrar CHECK (horaCerrar >= 0 AND horaCerrar <= 23),
     ciudadId INT,
-    FOREIGN KEY(ciudadId) REFERENCES Ciudad(id)
+    FOREIGN KEY(ciudadId) REFERENCES Ciudad(id),
     PRIMARY KEY(id)
 );
 
@@ -40,30 +40,30 @@ CREATE TABLE Sucursal(
 CREATE TABLE Empleado(
     id INT,
     CI VARCHAR(20) UNIQUE,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50),
-    sexo CHAR(1),
+    sexo CHAR(1) CHECK (sexo IN ('M', 'F')),
     direccionCorta VARCHAR(50),
     cargoId INT,
     empleadoSupervisorId INT,
     sucursalId INT,
     fechaContrato DATE,
-    bonoFijoMensual DECIMAL(10,2),
-    horalnicio TIME,
-    horaFin TIME,
-    cantidadDiasTrabajoPorSemana INT,
-    FOREIGN KEY(cargoId) REFERENCES Cargo(id)
-    FOREIGN KEY(empleadoSupervisorId) REFERENCES Empleado(id)
-    FOREIGN KEY(sucursalId) REFERENCES Sucursal(id)
+    bonoFijoMensual DECIMAL(10,2) CHECK (bonoFijoMensual >= 0),
+    horalnicio INT CHECK (horaInicio >= 0 AND horaInicio <= 23),
+    horaFin INT CHECK (horaFin >= 0 AND horaFin <= 23),
+    cantidadDiasTrabajoPorSemana INT CHECK CHECK (cantidadDiasTrabajoPorSemana >= 1 AND cantidadDiasTrabajoPorSemana <= 7),
+    FOREIGN KEY(cargoId) REFERENCES Cargo(id),
+    FOREIGN KEY(empleadoSupervisorId) REFERENCES Empleado(id),
+    FOREIGN KEY(sucursalId) REFERENCES Sucursal(id),
     PRIMARY KEY(id)
 );
 
 -- Cargos 
 CREATE TABLE Cargo(
     id INT,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
     descripcion TEXT,
-    salarioBasePorHora DECIMAL(10,2)
+    salarioBasePorHora DECIMAL(10,2) CHECK (salarioBasePorHora >= 0),
     PRIMARY KEY(id)
 );
 
@@ -71,8 +71,8 @@ CREATE TABLE Cargo(
 CREATE TABLE Inventario(
     id INT,
     productoId INT,
-    cantidad INT,
-    FOREIGN KEY(productoId) REFERENCES Producto(id)
+    cantidad INT CHECK (cantidad >= 0),
+    FOREIGN KEY(productoId) REFERENCES Producto(id),
     PRIMARY KEY(id)
 );
 
@@ -80,12 +80,12 @@ CREATE TABLE Inventario(
 CREATE TABLE Proveedor(
     id INT,
     RIF VARCHAR(20) UNIQUE,
-    nombre VARCHAR(50),
+    nombre VARCHAR(50) NOT NULL,
     contacto VARCHAR(50),
     telefono VARCHAR(20),
     correo VARCHAR(100),
     ciudadId INT,
-    FOREIGN KEY(ciudadId) REFERENCES Ciudad(id)
+    FOREIGN KEY(ciudadId) REFERENCES Ciudad(id),
     PRIMARY KEY(id)
 );
 
@@ -95,9 +95,9 @@ CREATE TABLE ProveedorProducto(
     proveedorId INT,
     productoId INT,
     fechaCompra DATE,
-    precioPor DECIMAL(10,2),
-    cantidad INT,
-    FOREIGN KEY(proveedorId) REFERENCES Proveedor(id)
-    FOREIGN KEY(productoId) REFERENCES Producto(id)
+    precioPor DECIMAL(10,2) CHECK (precioPor >= 0),
+    cantidad INT CHECK (cantidad >= 0),
+    FOREIGN KEY(proveedorId) REFERENCES Proveedor(id),
+    FOREIGN KEY(productoId) REFERENCES Producto(id),
     PRIMARY KEY(id)
 );
