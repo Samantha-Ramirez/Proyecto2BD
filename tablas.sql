70c15cbb9e2b43b258e3f385618b639c2b89dce2
Juan David <juandavid2002111@gmail.com>
2025-02-27 22:51:02 -0400

    juan tablas

--- tablas.sql
+++ tablas.sql
@@ -101,3 +101,109 @@ CREATE TABLE ProveedorProducto(
     FOREIGN KEY(productoId) REFERENCES Producto(id),
     PRIMARY KEY(id)
 );
+
+-- Cliente
+CREATE TABLE Cliente (
+    id INT PRIMARY KEY,
+    CI VARCHAR(20) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    correo VARCHAR(50) NOT NULL,
+    sexo VARCHAR(1) NOT NULL CHECK (sexo IN ('M', 'F')),
+    fechaNacimiento DATE NOT NULL,
+    fechaRegistro DATETIME NOT NULL
+);
+
+-- ClienteDirección
+CREATE TABLE ClienteDirección (
+    id INT PRIMARY KEY,
+    clienteld INT NOT NULL,
+    tipoDireccion VARCHAR(50) NOT NULL CHECK (tipoDireccion IN ('Facturación', 'Envío')),
+    dirLinea1 VARCHAR(50) NOT NULL,
+    ciudadId INT NOT NULL,
+    FOREIGN KEY (clienteld) REFERENCES Cliente(id),
+    FOREIGN KEY (ciudadId) REFERENCES Ciudad(id)
+);
+
+-- Categoria
+CREATE TABLE Categoria (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripción VARCHAR(50)
+);
+
+-- Marca
+CREATE TABLE Marca (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripción VARCHAR(50)
+);
+
+-- Producto
+CREATE TABLE Producto (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    codigoBarra VARCHAR(50) NOT NULL,
+    descripción VARCHAR(50),
+    tipoPrecio VARCHAR(50) NOT NULL CHECK (tipoPrecio IN ('PorUnidad', 'PorPesoKg')),
+    precioPor DECIMAL(10, 2) NOT NULL CHECK (precioPor >= 0),
+    esExentoIVA BIT NOT NULL,
+    categoriald INT NOT NULL,
+    marcald INT NOT NULL,
+    FOREIGN KEY (categoriald) REFERENCES Categoria(id),
+    FOREIGN KEY (marcald) REFERENCES Marca(id)
+);
+
+-- ProductoRecomendadoParaProducto
+CREATE TABLE ProductoRecomendadoParaProducto (
+    productold INT NOT NULL,
+    productoRecomendadold INT NOT NULL,
+    mensaje VARCHAR(50),
+    PRIMARY KEY (productold, productoRecomendadold),
+    FOREIGN KEY (productold) REFERENCES Producto(id),
+    FOREIGN KEY (productoRecomendadold) REFERENCES Producto(id)
+);
+
+-- ProductoRecomendadoParaCliente
+CREATE TABLE ProductoRecomendadoParaCliente (
+    clienteId INT NOT NULL,
+    productoRecomendadold INT NOT NULL,
+    fechaRecomendacion DATETIME NOT NULL,
+    mensaje VARCHAR(50),
+    PRIMARY KEY (clienteId, productoRecomendadold),
+    FOREIGN KEY (clienteId) REFERENCES Cliente(id),
+    FOREIGN KEY (productoRecomendadold) REFERENCES Producto(id)
+);
+
+
+
+-- HistoriaClienteProducto
+CREATE TABLE HistoriaClienteProducto (
+    clienteId INT NOT NULL,
+    productold INT NOT NULL,
+    fecha DATETIME NOT NULL,
+    tipoAccion VARCHAR(50) NOT NULL CHECK (tipoAccion IN ('Búsqueda', 'Carrito', 'Compra')),
+    PRIMARY KEY (clienteId, productold, fecha),
+    FOREIGN KEY (clienteId) REFERENCES Cliente(id),
+    FOREIGN KEY (productold) REFERENCES Producto(id)
+);
+
+-- Carrito
+CREATE TABLE Carrito (
+    clienteId INT NOT NULL,
+    productold INT NOT NULL,
+    fechaAgregado DATETIME NOT NULL,
+    cantidad INT NOT NULL CHECK (cantidad >= 0),
+    precioPor DECIMAL(10, 2) NOT NULL CHECK (precioPor >= 0),
+    PRIMARY KEY (clienteId, productold, fechaAgregado),
+    FOREIGN KEY (clienteId) REFERENCES Cliente(id),
+    FOREIGN KEY (productold) REFERENCES Producto(id)
+);
+
+-- TipoEnvio
+CREATE TABLE TipoEnvio (
+    id INT PRIMARY KEY,
+    nombreEnvío VARCHAR(50) NOT NULL,
+    tiempoEstimadoEntrega INT NOT NULL CHECK (tiempoEstimadoEntrega >= 0 AND tiempoEstimadoEntrega <= 23),
+    costoEnvío DECIMAL(10, 2) NOT NULL CHECK (costoEnvío >= 0)
+);
